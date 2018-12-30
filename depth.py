ce349854ee76140d8e67da56281c438281a02a72
giogia <Giovannitommasi95@gmail.com>
2018-12-30 15:57:50 +0800

    typo

--- depth.py
+++ depth.py
@@ -42,7 +42,7 @@ for i in range(1,54):
 
     for j in range(5):
 
-        img = Image.open('../result/res_panofull_ts_box_joint/img/' + str(i) + '-' + str(j) + '.png')
+        img = Image.open('../result/res_panofull_ts_box_joint/persp/' + str(i) + '-' + str(j) + '.png')
         depth = depth(img)
         depth.save('../result/res_panofull_ts_box_joint/depth/' + str(i) + '-' + str(j) + 'd.png')
 
